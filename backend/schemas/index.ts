0d5d749d7d4c5ec383ff1b57f5a0dd7e5a43a20c
mahmoud <mahmoudhammad3009@gmail.com>
2023-03-28 23:27:30 +0300

    create file structure for backend folder

---
+++ backend/schemas/index.ts
@@ -0,0 +1,18 @@
+import about from './about'
+import brands from './brands'
+import contact from './contact'
+import skills from './skills'
+import experiences from './experiences'
+import testimonials from './testimonials'
+import workExperience from './workExperience'
+import works from './works'
+export const schemaTypes = [
+  testimonials,
+  about,
+  brands,
+  contact,
+  skills,
+  experiences,
+  workExperience,
+  works,
+]
